2aacd6b12fc6b48c71fc56d72713f3745c31ca97
demoss1306 <demoss@tut.by>
2015-04-08 23:25:34 +0300

    Update Candidate.java

--- Candidate.java
+++ Candidate.java
@@ -1,1 +1,19 @@
 public class Candidate{
+  private String name;
+  private int voices = 0;
+  public Candidate (String name){
+    this.name = name;
+  }
+  
+  public String getName (){
+    return name;
+  }
+  
+  public void addVoice(){
+    voices += 1;
+  }
+  
+  public int getVoices(){
+    return voices;
+  }
+}
