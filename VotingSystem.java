6860bfe5c1f7996cc1deb76d6ec54d021d10bb4b
demoss1306 <demoss@tut.by>
2015-04-09 00:15:18 +0300

    Update VotingSystem.java

--- VotingSystem.java
+++ VotingSystem.java
@@ -1,1 +1,29 @@
-
+public class VotingSystem{
+  List<User> users;
+  Voting currentVoting;
+  User currentUser;
+  
+  public static void main(String[] args){
+    
+  }
+  
+  User addUser (String name, String login, String password, String repeation){
+    
+  }
+  
+  User findUser (String login, String password){
+    
+  }
+  
+  save(){
+    
+  }
+  
+  load (){
+    
+  }
+  
+  List getResults(){
+    
+  }
+}
